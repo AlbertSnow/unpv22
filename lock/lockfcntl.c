0d0236a46ba6222e3b7c83d583bcbe4d0a358dc9
Jefung <jefung865424525@gmail.com>
2018-11-11 12:06:24 +0800

    add record/file lock comment

--- lock/lockfcntl.c
+++ lock/lockfcntl.c
@@ -1,27 +1,23 @@
-#include	"unpipc.h"
+#include "unpipc.h"
 
-void
-my_lock(int fd)
-{
-	struct flock	lock;
+void my_lock(int fd) {
+    struct flock lock;
 
-	lock.l_type = F_WRLCK;
-	lock.l_whence = SEEK_SET;
-	lock.l_start = 0;
-	lock.l_len = 0;				/* write lock entire file */
+    lock.l_type = F_WRLCK;
+    lock.l_whence = SEEK_SET;
+    lock.l_start = 0;
+    lock.l_len = 0; /* write lock entire file */
 
-	Fcntl(fd, F_SETLKW, &lock);
+    Fcntl(fd, F_SETLKW, &lock);
 }
 
-void
-my_unlock(int fd)
-{
-	struct flock	lock;
+void my_unlock(int fd) {
+    struct flock lock;
 
-	lock.l_type = F_UNLCK;
-	lock.l_whence = SEEK_SET;
-	lock.l_start = 0;
-	lock.l_len = 0;				/* unlock entire file */
+    lock.l_type = F_UNLCK;
+    lock.l_whence = SEEK_SET;
+    lock.l_start = 0;
+    lock.l_len = 0; /* unlock entire file */
 
-	Fcntl(fd, F_SETLK, &lock);
+    Fcntl(fd, F_SETLK, &lock);
 }
--- lock/locknone.c
+++ lock/locknone.c
@@ -1,11 +1,8 @@
-void
-my_lock(int fd)
-{
-	return;
+// 不上锁
+void my_lock(int fd) {
+    return;
 }
 
-void
-my_unlock(int fd)
-{
-	return;
+void my_unlock(int fd) {
+    return;
 }
